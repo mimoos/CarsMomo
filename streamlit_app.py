7dc9b28db26db544ef7ee3355d628d1a8765274e
mimoos <mimoos@users.noreply.github.com>
2024-06-23 18:19:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ column_Model = label_encoder.fit_transform(Car_data['Model'])
 
 ## Car_model = label_encoder.transform([Car_model])
 
-Car_data_new = Car_data.drop(['Make', 'Model], axis=1)
+Car_data_new = Car_data.drop(['Make', 'Model'], axis=1)
 Car_data_new
 
 """
