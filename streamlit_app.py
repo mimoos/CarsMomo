98092f0cf56aac0dba4e9cdaca4a0cf99c17f675
mimoos <mimoos@users.noreply.github.com>
2024-06-23 15:55:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ List_year.sort()
 Car_make = st.selectbox('Car Make:', options=List_make, index=None)
 Car_model = st.selectbox('Car Model:', options=List_model, index=None)
 Car_year = st.selectbox('Car Year:', options=List_year, index=None)
-Car_mileage = st.text_input('Car Mileage:')
-Car_mileage = int(Car_mileage, value=None)
+Car_mileage = st.text_input('Car Mileage:', value=None)
+Car_mileage = int(Car_mileage)
 
 
 
