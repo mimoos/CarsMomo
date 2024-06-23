ae95fbeffa2daf7d9bbb67a3a8d01d48a12575de
mimoos <mimoos@users.noreply.github.com>
2024-06-23 19:47:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,11 +36,10 @@ column_Make = column_Make.drop(['Price', 'Year', 'Model', 'Mileage'], axis=1)
 label_encoder = preprocessing.LabelEncoder()
 column_Model = label_encoder.fit_transform(Car_data['Model'])
 column_Model = pd.DataFrame(column_Model, columns = ['Model'])
-column_Model
 
-Car_model
-Car_model = label_encoder.transform([Car_model])
-Car_model
+## Car_model = label_encoder.transform([Car_model])
+
+
 
 Car_data_new = Car_data.drop(['Make', 'Model'], axis=1)
 Car_data_new = pd.concat([Car_data_new.reset_index(drop=True), column_Make.reset_index(drop=True), column_Model.reset_index(drop=True)], axis=1)
