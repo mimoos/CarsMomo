016c732349efa3babe967345c8dccf7fe1f8a786
mimoos <mimoos@users.noreply.github.com>
2024-06-23 19:41:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ Car_mileage = st.text_input('Car Mileage:', value=None)
 
 ## columns = pd.get_dummies(data=Car_data, columns=['Make', 'Model'])        too large data >200mb
 column_Make = pd.get_dummies(data=Car_data, columns=['Make'])
-
+column_Make = column_Make.drop(['Price', 'Year', 'Model', 'Mileage'], axis=1)
 
 label_encoder = preprocessing.LabelEncoder()
 column_Model = label_encoder.fit_transform(Car_data['Model'])
@@ -40,9 +40,8 @@ column_Model
 
 ## Car_model = label_encoder.transform([Car_model])
 
-## Car_data_new = Car_data.drop(['Make', 'Model'], axis=1)
-Car_data_new = Car_data
-Car_data_new = pd.concat([column_Make.reset_index(drop=True), column_Model.reset_index(drop=True)], axis=1)
+Car_data_new = Car_data.drop(['Make', 'Model'], axis=1)
+Car_data_new = pd.concat([Car_data_new.reset_index(drop=True), column_Make.reset_index(drop=True), column_Model.reset_index(drop=True)], axis=1)
 
 Car_data_new
 
