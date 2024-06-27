4262413f466b1538403e87d84b2a0107af3d5e96
mimoos <mimoos@users.noreply.github.com>
2024-06-27 04:23:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,15 @@ st.button('Predict the price')
 column_Make = pd.get_dummies(data=Car_data, columns=['Make'])
 column_Make = column_Make.drop(['Price', 'Year', 'Model', 'Mileage'], axis=1)
 
+column_Model = Car_data['Model']
+encoder = OneHotEncoder(sparse=False)
+car_models_2d = np.array(column_Model).reshape(-1, 1)
+encoded = encoder.fit_transform(car_models_2d)
+luxury_encoded = encoder.transform(encoded)
+luxury_bool = np.sum(luxury_encoded, axis=1, dtype=bool)
+
+luxury_bool
+
 #column_Model = Car_data['Model']
 #encoder = OneHotEncoder()
 #ct = ColumnTransformer(transformers=[('onehot_categorical', OneHotEncoder(), [0])], remainder='passthrough')
@@ -47,11 +56,11 @@ column_Make = column_Make.drop(['Price', 'Year', 'Model', 'Mileage'], axis=1)
 
 ## label_encoder = preprocessing.LabelEncoder()
 ## column_Model = label_encoder.fit_transform(Car_data['Model'])
-One_Hot_Encoder = preprocessing.OneHotEncoder(categories='auto')
-column_Model = Car_data['Model']
-ct = ColumnTransformer(transformers=[('onehot_categorical', OneHotEncoder(), [0])], remainder='passthrough')
+##1 One_Hot_Encoder = preprocessing.OneHotEncoder(categories='auto')
+##2column_Model = Car_data['Model']
+##3ct = ColumnTransformer(transformers=[('onehot_categorical', OneHotEncoder(), [0])], remainder='passthrough')
 ## column_Model = One_Hot_Encoder.fit_transform(Car_data['Model'].values.reshape(-1, 1))
-column_Model = np.array(ct.fit_transform(column_Model.values.reshape(1, -1)))
+##4column_Model = np.array(ct.fit_transform(column_Model.values.reshape(1, -1)))
 ## column_Model = pd.DataFrame(column_Model, columns=One_Hot_Encoder.get_feature_names_out(Car_data['Model']))
 ## column_Model = pd.DataFrame(column_Model)
 ## column_Model = np.array(column_Model, dtype=int)
