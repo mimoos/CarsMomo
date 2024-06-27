185a571f9b6dc03b756c184fc55552813275c817
mimoos <mimoos@users.noreply.github.com>
2024-06-26 22:37:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ column_Make = column_Make.drop(['Price', 'Year', 'Model', 'Mileage'], axis=1)
 ## column_Model = label_encoder.fit_transform(Car_data['Model'])
 One_Hot_Encoder = preprocessing.OneHotEncoder(categories='auto')
 column_Model = Car_data['Model']
-ct = ColumnTransformer(transformers=[('onehot_categorical', OneHotEncoder(), 0)], remainder='passthrough')
+ct = ColumnTransformer(transformers=[('onehot_categorical', OneHotEncoder(), [0])], remainder='passthrough')
 ## column_Model = One_Hot_Encoder.fit_transform(Car_data['Model'].values.reshape(-1, 1))
 column_Model = ct.fit_transform(column_Model.to_numpy())
 ## column_Model = pd.DataFrame(column_Model, columns=One_Hot_Encoder.get_feature_names_out(Car_data['Model']))
