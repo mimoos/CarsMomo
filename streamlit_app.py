88e0c0580349adf677ba3934da4d12cf44a6ca26
mimoos <mimoos@users.noreply.github.com>
2024-07-02 21:26:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ Car_data['Model'] = Car_data['Model'].str.replace(',', '')
 
 #fig = go.Figure()
 #data=[go.Bar(x=[1, 2, 3], y=[1, 3, 2])]
-st.bar_chart(data=Car_data, x=Car_data['Year','Mileage'], y=Car_data['Price'], x_label='Year and Mileage', y_label='Price', color=None, horizontal=False, width=12, height=8, use_container_width=True)
+st.bar_chart(data=Car_data, x=Car_data('Year','Mileage'), y=Car_data('Price'), x_label='Year and Mileage', y_label='Price', color=None, horizontal=False, width=12, height=8, use_container_width=True)
 
 
 
