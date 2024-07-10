e5bbb2cea417daa0d7a5de29cb76b391bb7c6688
mimoos <mimoos@users.noreply.github.com>
2024-07-09 20:34:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,9 @@ Car_make = st.selectbox('Car Make:', options=List_make, index=None)
 Car_model = st.selectbox('Car Model:', options=List_model, index=None)
 Car_year = st.selectbox('Car Year:', options=List_year, index=None)
 Car_mileage = st.text_input('Car Mileage:', value=None)
+if Car_mileage == '':
+    Car_mileage = '0'
+
 
 Car_button = st.button('Predict the price')
 
